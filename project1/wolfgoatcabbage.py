d6a8cceb0996f66cf2e308af74a722defa66f08d
Austin S <56807941+austin-sohn@users.noreply.github.com>
2022-02-22 18:47:28 -0800

    update 1

--- project1/wolfgoatcabbage.py
+++ project1/wolfgoatcabbage.py
@@ -3,22 +3,41 @@
 # 3/4/22
 
 from search import *
-def goal_test(state):
+class WolfGoatCabbage():
+    def __init__(self, initial = {'F', 'G', 'W', 'C'}, goal = {}):
+        self.initial = initial
+        self.goal = goal
+        self.state = initial
 
-    return True
-def result(state, action):
-    newstate = state
-    return newstate
-def actions(state):
-    act_list = []
-    return act_list
-def WolfGoatCabbage():
+    def goal_test(self, state):
+        goal = False
+        if state == self.goal:
+            goal = True
+        else:
+            goal = False
+        return goal
 
-    return 1
+    def result(self, state, action):
+    # In result() you will have to return the next state as a frozenset since the search algorithms
+    # require the state to be represented as a hashable data type.
+        if state == {'F', 'G', 'W', 'C'}:
+            state.remove('W','C')
+        if state == {'G', 'F'}:
+            state.remove('G')
+        froze = frozenset(state)
+        return froze
+
+    def actions(self, state):
+        print(state)
+        act_list = []
+        return act_list
 
 if __name__ == '__main__':
     wgc = WolfGoatCabbage()
+    wgc.actions(wgc.state)
+    """
     solution = depth_first_graph_search(wgc).solution()
     print(solution)
     solution = breadth_first_graph_search(wgc).solution()
     print(solution)
+    """
