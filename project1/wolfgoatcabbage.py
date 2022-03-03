ed9cb216457520fa8b1eb9388e5b7516a504d8fc
Austin S <56807941+austin-sohn@users.noreply.github.com>
2022-03-02 23:47:38 -0800

    3rd

--- project1/wolfgoatcabbage.py
+++ project1/wolfgoatcabbage.py
@@ -3,45 +3,60 @@
 # 3/4/22
 
 from search import *
-class WolfGoatCabbage():
-    def __init__(self, initial, goal = {'F', 'G', 'W', 'C'}):
-        super().__init__(initial, goal)
+class WolfGoatCabbage(Problem):
+    def __init__(self, initial = {}, goal = {'F', 'G', 'W', 'C'}):
+        self.initial = initial
+        self.goal = goal
+        self.state = initial
 
     def goal_test(self, state):
         goal = False
         if state == self.goal:
             goal = True
-        else:
-            goal = False
         return goal
 
     def result(self, state, action):
     # In result() you will have to return the next state as a frozenset since the search algorithms
     # require the state to be represented as a hashable data type.
-        if state == {'F', 'G', 'W', 'C'}:
-            state.remove('W','C')
-        if state == {'G', 'F'}:
-            state.remove('G')
-        froze_state = frozenset(state)
-        return froze_state
+        if state == {'F', 'W', 'G', 'C'}:
+            state.remove('F','G')
+        if state == {'W', 'C'}:
+            state.add('F')
+        if state == {'F', 'W', 'C'}:
+            state.remove('F', 'W')
+        if state == {'C'}:
+            state.add('F', 'G')
+        if state == {'F', 'G', 'C'}:
+            state.remove('F', 'C')
+        if state == {'G'}:
+            state.add('F')
+        if state == {'F', 'G'}:
+            state.remove('F', 'G')
+        froze = frozenset(state)
+        return froze
 
     def actions(self, state):
-        pot_act = {'F', 'G', 'W', 'C'}
-        if 'F' in state:
-            pot_act.remove('F')
-        if 'G' in state:
-            pot_act.remove('G')
-        if 'W' in state:
-            pot_act.remove('W')
-        if 'C' in state:
-            pot_act.remove('C')
-        return pot_act
+        print(state)
+        act_list = []
+        if state == {'F', 'W', 'G', 'C'}:
+            act_list = [{'F', 'W'}, {'F', 'G'}, {'F', 'C'}]
+        if state == {'W', 'C'}:
+            act_list = [{'F'}, {'F', 'G'}]
+        if state == {'F', 'W', 'C'}:
+            act_list = [{'F', 'W'}, {'F', 'C'}]
+        if state == {'F', 'G', 'C'} or state == {'F', 'W', 'G'}:
+            act_list = [{'F', 'W'}]
+        if state == {'G'}:
+            act_list = [{'F'}]
+        if state == {'F', 'G'}:
+            act_list == [{'F', 'G'}]
+        return act_list
 
 if __name__ == '__main__':
     wgc = WolfGoatCabbage()
-    """
+    # wgc.actions(wgc.state)
+    # print(wgc.result)
     solution = depth_first_graph_search(wgc).solution()
     print(solution)
-    solution = breadth_first_graph_search(wgc).solution()
-    print(solution)
-    """
+    # solution = breadth_first_graph_search(wgc).solution()
+    # print(solution)
