5109846cf9eac5af1c8916de89c1a875df254d8e
Austin S <56807941+austin-sohn@users.noreply.github.com>
2022-03-03 21:47:54 -0800

    5th

--- project1/wolfgoatcabbage.py
+++ project1/wolfgoatcabbage.py
@@ -16,44 +16,39 @@ class WolfGoatCabbage(Problem):
     def result(self, state, action):
     # In result() you will have to return the next state as a frozenset since the search algorithms
     # require the state to be represented as a hashable data type.
-        if state == {'F', 'W', 'G', 'C'}:
-            state.remove('F','G')
-        if state == {'W', 'C'}:
-            state.add('F')
-        if state == {'F', 'W', 'C'}:
-            state.remove('F', 'W')
-        if state == {'C'}:
-            state.add('F', 'G')
-        if state == {'F', 'G', 'C'}:
-            state.remove('F', 'C')
-        if state == {'G'}:
-            state.add('F')
-        if state == {'F', 'G'}:
-            state.remove('F', 'G')
-        froze = frozenset(state)
-        return froze
+        print(state)
+        print(action)
+        # assumes action is valid
+        if action.issubset(state):
+            state = state.difference(action)
+        else:
+            state = state.union(action)
+        #froze_st = frozenset(state)
+        #return froze_st
+        return state
 
     def actions(self, state):
-        print(state)
         act_list = []
         if state == {'F', 'W', 'G', 'C'}:
-            act_list = [{'F', 'W'}, {'F', 'G'}, {'F', 'C'}]
-        if state == {'W', 'C'}:
+            act_list = [{'F', 'G'}]
+        elif state == {'W', 'C'}:
             act_list = [{'F'}, {'F', 'G'}]
-        if state == {'F', 'W', 'C'}:
+        elif state == {'F', 'W', 'C'}:
             act_list = [{'F', 'W'}, {'F', 'C'}]
-        if state == {'F', 'G', 'C'} or state == {'F', 'W', 'G'}:
+        elif state == {'C'} or state == {'W'}:
+            act_list = [{'F'}, {'F', 'G'}]
+        elif state == {'G'}:
+            act_list = [{'F'}, {'F', 'C'}, {'F', 'W'}]
+        elif state == {'F', 'G', 'C'}:
+            act_list = [{'F', 'C'}]
+        elif state == {'F', 'W', 'G'}:
             act_list = [{'F', 'W'}]
-        if state == {'G'}:
-            act_list = [{'F'}]
-        if state == {'F', 'G'}:
+        elif state == {'F', 'G'}:
             act_list == [{'F', 'G'}]
         return act_list
 
 if __name__ == '__main__':
     wgc = WolfGoatCabbage()
-    # wgc.actions(wgc.state)
-    # print(wgc.result)
     solution = depth_first_graph_search(wgc).solution()
     print(solution)
     # solution = breadth_first_graph_search(wgc).solution()
